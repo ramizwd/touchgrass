89dde22b71a6ca0b86419ffe0c3c8a98c1335efd
R <ramz9_97@hotmail.com>
2022-09-27 19:55:35 +0300

    DataStore preferences

--- app/src/main/java/com/example/touchgrass/MainActivity.kt
+++ app/src/main/java/com/example/touchgrass/MainActivity.kt
@@ -14,15 +14,26 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.floatPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import androidx.lifecycle.lifecycleScope
 import com.example.touchgrass.ui.Navigation
 import com.example.touchgrass.ui.theme.TouchgrassTheme
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 import java.text.DateFormat
+
+private const val STEPS_PREFERENCES_NAME = "steps"
 
 class MainActivity : ComponentActivity(), SensorEventListener {
     companion object {
         private lateinit var sensorManager: SensorManager
         private var stepCounter: Sensor? = null
     }
+
+    private val Context.dataStore by preferencesDataStore(name = STEPS_PREFERENCES_NAME)
+    private var totalSteps = 0f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -53,14 +64,16 @@ class MainActivity : ComponentActivity(), SensorEventListener {
         }
     }
 
-    override fun onSensorChanged(p0: SensorEvent?) {
-        p0 ?: return
-        p0.values.firstOrNull()?.let {
+    override fun onSensorChanged(event: SensorEvent?) {
+        event ?: return
+
+        event.values.firstOrNull()?.let {
             Log.d("TYPE_STEP_COUNTER", "Count: $it")
+            totalSteps = it
         }
 
         val lastDeviceBootTimeInMillis = System.currentTimeMillis() - SystemClock.elapsedRealtime()
-        val sensorEventTimeInNanos = p0.timestamp
+        val sensorEventTimeInNanos = event.timestamp
         val sensorEventTimeInMillis = sensorEventTimeInNanos / 1000_000
 
         val actualSensorEventTimeInMillis = lastDeviceBootTimeInMillis + sensorEventTimeInMillis
@@ -68,7 +81,40 @@ class MainActivity : ComponentActivity(), SensorEventListener {
         Log.d("TYPE_STEP_COUNTER", "Sensor triggered at $displayDateStr")
     }
 
-    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
-        Log.d("TYPE_STEP_COUNTER", "onAccuracyChanged Sensor: $p0, Accuracy: $p1")
+    override fun onAccuracyChanged(sensor: Sensor?, p1: Int) {
+        Log.d("TYPE_STEP_COUNTER", "onAccuracyChanged Sensor: $sensor, Accuracy: $p1")
+    }
+
+    /**
+     * DataStore functions for saving TYPE_STEP_COUNTER sensor data
+     */
+
+    private suspend fun saveData(key: String, value: Float) {
+        val dataStoreKey = floatPreferencesKey(key)
+        dataStore.edit { steps ->
+            steps[dataStoreKey] = value
+        }
+    }
+
+    private suspend fun loadData(key: String): Float? {
+        val dataStoreKey = floatPreferencesKey(key)
+        val preferences = dataStore.data.first()
+        return preferences[dataStoreKey]
+    }
+
+    override fun onPause() {
+        super.onPause()
+        lifecycleScope.launch {
+            saveData(STEPS_PREFERENCES_NAME, totalSteps)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        lifecycleScope.launch {
+            val savedSteps = loadData(STEPS_PREFERENCES_NAME)
+            totalSteps = savedSteps ?: 0f
+            Log.d("TYPE_STEP_COUNTER", "Saved steps: $savedSteps")
+        }
     }
 }
