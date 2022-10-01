57ff8e572f087a054b2f6360a53d2c54ce382c58
R <ramz9_97@hotmail.com>
2022-10-02 00:41:08 +0300

    created handler for timer and cleaned steps sensor and DataStore logic

--- app/src/main/java/com/example/touchgrass/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/touchgrass/ui/home/HomeViewModel.kt
@@ -5,26 +5,10 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class HomeViewModel: ViewModel() {
-    private val _hour: MutableLiveData<Int> = MutableLiveData()
-    val currentHour: LiveData<Int> = _hour
+    private val _minute: MutableLiveData<Int> = MutableLiveData()
+    val currentTotalMinutes: LiveData<Int> = _minute
 
-    fun onHourUpdate(hour: Int) {
-        _hour.value = hour
-
-//            when(hour) {
-//            0 -> 0
-//            13 -> 10
-//            14 -> 20
-//            15 -> 30
-//            16 -> 40
-//            17 -> 50
-//            18 -> 60
-//            19 -> 70
-//            20 -> 80
-//            21 -> 90
-//            22 -> 95
-//            23 -> 99
-//            else -> 0
-//        }
+    fun onHourUpdate(totalMinutesOfDay: Int) {
+        _minute.value = totalMinutesOfDay
     }
 }
