8d4ac92b9bdb24290fe196fc6e66739ef449a92a
R <63073421+ramizwd@users.noreply.github.com>
2022-10-11 23:55:51 +0300

    created a service for step counter sensor and refactored some code

--- app/src/main/java/com/example/touchgrass/TouchGrassApp.kt
+++ app/src/main/java/com/example/touchgrass/TouchGrassApp.kt
@@ -4,15 +4,18 @@ import android.app.Application
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
+import com.example.touchgrass.utils.Constants
+import com.example.touchgrass.utils.Constants.NOTIFICATION_CHANNEL_ID
+import com.example.touchgrass.utils.Constants.NOTIFICATION_CHANNEL_NAME
 
 class TouchGrassApp: Application() {
 
     override fun onCreate() {
         super.onCreate()
         val channel = NotificationChannel(
-            "steps",
-            "Steps",
-            NotificationManager.IMPORTANCE_LOW
+            NOTIFICATION_CHANNEL_ID,
+            NOTIFICATION_CHANNEL_NAME,
+            NotificationManager.IMPORTANCE_HIGH
         )
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         notificationManager.createNotificationChannel(channel)
