6d15e790ccfa9f1b998ab4966ef90f4129f5c2e7
R <63073421+ramizwd@users.noreply.github.com>
2022-10-13 20:55:38 +0300

    CircularProgressBar colors changed

--- app/src/main/java/com/example/touchgrass/ui/theme/Color.kt
+++ app/src/main/java/com/example/touchgrass/ui/theme/Color.kt
@@ -2,8 +2,9 @@ package com.example.touchgrass.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-
 val SCGreen = Color(0xFF8ACB88)
 val SCDarkGreen = Color(0xFF5A9A5A)
 val SCOrange = Color(0xFFF0A202)
-val SCLightOrange = Color(0xFFFFFF7B)
+//val SCLightOrange = Color(0xFFFFFF7B)
+//val SCYellow = Color(0xFFDDFC74)
+//val SCBlue = Color(0xFF48C1EC)
