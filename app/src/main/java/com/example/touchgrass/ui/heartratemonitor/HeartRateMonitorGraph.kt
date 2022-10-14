782b222adf3baf2526ef3b6fee3b45940249e85b
R <63073421+ramizwd@users.noreply.github.com>
2022-10-14 05:18:24 +0300

    fixed dark mode colors

--- app/src/main/java/com/example/touchgrass/ui/heartratemonitor/HeartRateMonitorGraph.kt
+++ app/src/main/java/com/example/touchgrass/ui/heartratemonitor/HeartRateMonitorGraph.kt
@@ -7,6 +7,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.viewinterop.AndroidView
@@ -21,14 +22,27 @@ import com.github.mikephil.charting.data.LineDataSet
 fun HeartRateGraph(hr: Float?, sec: Float?) {
 
     val dataSets = remember { mutableStateListOf<Entry>() }
-    if (hr != null && hr != 0f) {
-        dataSets.add(Entry(sec ?: 0f, hr))
-    }
+//    if (hr != null && hr != 0f) {
+//        dataSets.add(Entry(sec ?: 0f, hr))
+//    }
 
     val label = "Beats Per Minute"
     val lineColor = MaterialTheme.colors.secondary.toArgb()
     val noDataTextColor = MaterialTheme.colors.onPrimary.toArgb()
     val noDataText = stringResource(R.string.no_hr_data)
+    val themeTextColor = MaterialTheme.colors.onPrimary.toArgb()
+
+
+    dataSets.add(Entry(1f, 2f))
+    dataSets.add(Entry(2f, 3f))
+    dataSets.add(Entry(3f, 50f))
+    dataSets.add(Entry(4f, 2f))
+    dataSets.add(Entry(5f, 19f))
+    dataSets.add(Entry(6f, 20f))
+    dataSets.add(Entry(7f, 41f))
+    dataSets.add(Entry(8f, 4f))
+    dataSets.add(Entry(9f, 12f))
+    dataSets.add(Entry(10f, 2f))
 
     AndroidView(
         modifier = Modifier.fillMaxSize(),
@@ -55,12 +69,14 @@ fun HeartRateGraph(hr: Float?, sec: Float?) {
                 xAxis.apply {
                     isGranularityEnabled = true
                     position = XAxisPosition.BOTTOM
+                    textColor = themeTextColor
                     setDrawGridLines(false)
                     setDrawAxisLine(false)
                     setExtraOffsets(0f, 0f, 0f, 10f)
                 }
                 axisLeft.apply {
                     isGranularityEnabled = true
+                    textColor = themeTextColor
                     setDrawGridLines(false)
                 }
                 axisRight.apply {
@@ -68,6 +84,7 @@ fun HeartRateGraph(hr: Float?, sec: Float?) {
                     setDrawGridLines(false)
                     setDrawAxisLine(false)
                 }
+                legend.textColor = themeTextColor
                 description = null
                 data = lineData
                 setNoDataText(noDataText)
