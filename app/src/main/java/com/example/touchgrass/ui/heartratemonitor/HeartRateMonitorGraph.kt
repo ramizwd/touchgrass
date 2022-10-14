1425bff4f39f715ce7c2619147cf417e3dc20323
R <63073421+ramizwd@users.noreply.github.com>
2022-10-14 04:43:25 +0300

    heart rate graph UI completed

--- app/src/main/java/com/example/touchgrass/ui/heartratemonitor/HeartRateMonitorGraph.kt
+++ app/src/main/java/com/example/touchgrass/ui/heartratemonitor/HeartRateMonitorGraph.kt
@@ -8,14 +8,14 @@ import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.viewinterop.AndroidView
+import com.example.touchgrass.R
 import com.github.mikephil.charting.charts.LineChart
 import com.github.mikephil.charting.components.XAxis.XAxisPosition
-import com.github.mikephil.charting.data.BarEntry
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
-import com.github.mikephil.charting.formatter.ValueFormatter
 
 @Composable
 fun HeartRateGraph(hr: Float?, sec: Float?) {
@@ -25,45 +25,39 @@ fun HeartRateGraph(hr: Float?, sec: Float?) {
         dataSets.add(Entry(sec ?: 0f, hr))
     }
 
-//    dataSets.add(Entry(1f, 2f))
-//    dataSets.add(Entry(2f, 3f))
-//    dataSets.add(Entry(3f, 50f))
-//    dataSets.add(Entry(4f, 2f))
-//    dataSets.add(Entry(4f, 19f))
-//    dataSets.add(Entry(4f, 20f))
-//    dataSets.add(Entry(4f, 41f))
-//    dataSets.add(Entry(4f, 4f))
-//    dataSets.add(Entry(4f, 12f))
-//    dataSets.add(Entry(4f, 2f))
-
-
     val label = "Beats Per Minute"
     val lineColor = MaterialTheme.colors.secondary.toArgb()
+    val noDataTextColor = MaterialTheme.colors.onPrimary.toArgb()
+    val noDataText = stringResource(R.string.no_hr_data)
 
-    AndroidView (
+    AndroidView(
         modifier = Modifier.fillMaxSize(),
         factory = { context: Context ->
             LineChart(context)
         },
         update = { lineChart ->
-            var lineDataSet: LineDataSet? = null
-            lineDataSet = LineDataSet(dataSets, label).apply {
-                color = lineColor
-                isHighlightEnabled = true
-                lineWidth = 2f
-                mode = LineDataSet.Mode.CUBIC_BEZIER
-                setDrawValues(false)
-                setDrawCircles(false)
-                setDrawHighlightIndicators(false)
+            var lineData: LineData? = null
+
+            if (dataSets.isNotEmpty()) {
+                val lineDataSet = LineDataSet(dataSets, label).apply {
+                    color = lineColor
+                    isHighlightEnabled = true
+                    lineWidth = 2f
+                    mode = LineDataSet.Mode.CUBIC_BEZIER
+                    setDrawValues(false)
+                    setDrawCircles(false)
+                    setDrawHighlightIndicators(false)
+                }
+                lineData = LineData(lineDataSet)
             }
-            val lineData = LineData(lineDataSet)
+
             lineChart.apply {
                 xAxis.apply {
                     isGranularityEnabled = true
                     position = XAxisPosition.BOTTOM
                     setDrawGridLines(false)
                     setDrawAxisLine(false)
-                    setExtraOffsets(0f,0f,0f,10f)
+                    setExtraOffsets(0f, 0f, 0f, 10f)
                 }
                 axisLeft.apply {
                     isGranularityEnabled = true
@@ -76,6 +70,8 @@ fun HeartRateGraph(hr: Float?, sec: Float?) {
                 }
                 description = null
                 data = lineData
+                setNoDataText(noDataText)
+                setNoDataTextColor(noDataTextColor)
                 invalidate()
             }
         }
