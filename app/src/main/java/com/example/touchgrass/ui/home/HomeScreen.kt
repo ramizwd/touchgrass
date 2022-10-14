abc660b63a161dc7658fccc6310e205fd7b6207c
Johnkai Cortez <73104201+Johnkai2196@users.noreply.github.com>
2022-10-14 21:57:19 +0300

    comments added and remove not used imports

--- app/src/main/java/com/example/touchgrass/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/touchgrass/ui/home/HomeScreen.kt
@@ -3,7 +3,6 @@ package com.example.touchgrass.ui.home
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
@@ -12,7 +11,6 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
--- app/src/main/java/com/example/touchgrass/ui/hydration/HydrationViewModel.kt
+++ app/src/main/java/com/example/touchgrass/ui/hydration/HydrationViewModel.kt
@@ -3,6 +3,9 @@ package com.example.touchgrass.ui.hydration
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 
+/**
+ * HydrationViewModel purpose is to save, update and change livedata
+ */
 class HydrationViewModel {
     private val _numberGoal: MutableLiveData<Int> = MutableLiveData()
     val numberGoal: LiveData<Int> = _numberGoal
@@ -13,22 +16,37 @@ class HydrationViewModel {
     private val _itemsAmount: MutableLiveData<Int> = MutableLiveData()
     val itemsAmount: LiveData<Int> = _itemsAmount
 
+    /**
+     * update the targeted goal to drink
+     */
     fun onNumberGoalUpdate(numberGoal: Int) {
         _numberGoal.value = numberGoal
     }
 
+    /**
+     * add the consume amount of liquid
+     */
     fun onDrankAmountPlus(drankAmount: Int) {
         _drankAmount.value = _drankAmount.value?.plus(drankAmount)
     }
 
+    /**
+     * load the drank amount to livedata
+     */
     fun onDrankAmountUpdate(drankAmount: Int) {
         _drankAmount.value = drankAmount
     }
 
+    /**
+     * update the item amount made
+     */
     fun onItemsAmountUpdate(itemsAmount: Int) {
         _itemsAmount.value = itemsAmount
     }
 
+    /**
+     * reduce item amount by 1
+     */
     fun onItemsAmountReduce() {
         _itemsAmount.value = _itemsAmount.value?.minus(1)
     }
